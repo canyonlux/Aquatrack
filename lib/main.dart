dab74d5fd0cb27510dfb78ca80b4544426dc9d20
Ruben garcia <rubenalma1984@hotmail.com>
2024-03-02 18:30:01 +0100

    modificacion pantalla inicio y boton estandar

--- lib/main.dart
+++ lib/main.dart
@@ -21,85 +21,57 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WelcomeScreen extends StatefulWidget {
-  @override
-  _WelcomeScreenState createState() => _WelcomeScreenState();
-}
-
-class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(seconds: 1),
-      vsync: this,
-    )..repeat(reverse: true);
-
-    _animation = Tween<double>(begin: 50, end: 5).animate(_controller)
-      ..addListener(() {
-        setState(() {});
-      });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF1F1F1), // Fondo Gris claro
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Transform.translate(
-              offset: Offset(0, _animation.value),
-              child: Image.asset('assets/images/aquatracklogo.webp', width: 1000 * 0.6, height: 1000 * 0.6),
+      body: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/Titulo.jpg',
+              fit: BoxFit.cover,
             ),
-            Text(
-              'AquaTrack',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF0077B6), // Azul Principal para el título
-              ),
-            ),
-            Padding(
+          ),
+          Center(
+            child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                'Tu guía para encontrar fuentes de agua durante tus actividades al aire libre',
+                'Encuentra fuentes de agua durante tus actividades al aire libre',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 16,
-                  color: Color(0xFF333333), // Texto Gris oscuro
+                  color: Color(0xFFFFFFFF), // Texto Blanco
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: FloatingActionButton(
+          ),
+          Positioned(
+            bottom: 50,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
                 },
-                 child: Icon(Icons.water_drop, size: 40, color: Color(0xFF90E0EF),), // Icono de Gota de Agua
-                backgroundColor: Color(0xFF0077B6),
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ), heroTag: 'big_fab',
-                materialTapTargetSize: MaterialTapTargetSize.padded,
+                child: Text(
+                  'INICIO',
+                  style: TextStyle(
+                    color: Colors.white, // Texto en blanco
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF00B4D8), // Azul Secundario para botones
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30), // Bordes redondeados
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15), // Espaciado interno
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
