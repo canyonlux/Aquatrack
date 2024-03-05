29d553ac11b95ef9773c33f0d71f6819e5a5842d
Ruben garcia <rubenalma1984@hotmail.com>
2024-03-05 14:33:26 +0100

    trabajo realizado en el buscador de fuentes en mappage. integrado buscador de la lista json y el ignore de acentgos mayusculas etc

--- lib/MapPage.dart
+++ lib/MapPage.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'Fuente.dart';
+import 'FuenteDetailPage.dart';
 import 'FuentesPage.dart';
 import 'UserProfilePage.dart';
 
@@ -10,8 +15,39 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  List<Fuente> _fuentes = [];
+  List<Fuente> _fuentesFiltradas = [];
   final TextEditingController _searchController = TextEditingController();
   int _currentIndex = 0;
+  final MapController _mapController = MapController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFuentes();
+    _searchController.addListener(_filterFuentes);
+  }
+  Future<void> _loadFuentes() async {
+    final jsondata = await rootBundle.loadString('assets/FuentesDeAgua_Almassora.json');
+    final list = json.decode(jsondata) as List<dynamic>;
+    setState(() {
+      _fuentes = list.map((item) => Fuente.fromJson(item)).toList();
+      _fuentesFiltradas = _fuentes;
+    });
+  }
+
+  void _filterFuentes() {
+    final query = removeDiacritics(_searchController.text.toLowerCase());
+    setState(() {
+      _fuentesFiltradas = _fuentes.where((fuente) {
+        // Normalizar municipio y estado para la comparación
+        final municipioNormalized = removeDiacritics(fuente.municipio.toLowerCase());
+        final estadoNormalized = removeDiacritics(fuente.estado.toLowerCase());
+
+        return municipioNormalized.contains(query) || estadoNormalized.contains(query);
+      }).toList();
+    });
+  }
 
   void _onItemTapped(int index) {
     if (index == _currentIndex) return; // Evita recargar la misma página
@@ -28,6 +64,13 @@ class _MapPageState extends State<MapPage> {
         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => UserProfilePage()));
         break;
     }
+  }
+  void _zoomIn() {
+    _mapController.move(_mapController.center, _mapController.zoom + 1);
+  }
+
+  void _zoomOut() {
+    _mapController.move(_mapController.center, _mapController.zoom - 1);
   }
 
   @override
@@ -52,8 +95,21 @@ class _MapPageState extends State<MapPage> {
           ),
         ),
       ),
-      body: FlutterMap(
-        mapController: MapController(),
+      body: _searchController.text.isNotEmpty ? ListView.builder(
+        itemCount: _fuentesFiltradas.length,
+        itemBuilder: (context, index) {
+          final fuente = _fuentesFiltradas[index];
+          return ListTile(
+            title: Text(fuente.direccion),
+            subtitle: Text(fuente.municipio),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => FuenteDetailPage(fuente: fuente)));
+            },
+          );
+        },
+      )
+      : FlutterMap(
+        mapController: _mapController,
         children: [
           TileLayer(
             urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -65,6 +121,25 @@ class _MapPageState extends State<MapPage> {
           zoom: 13.0,
         ),
       ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: _zoomIn,
+              child: Icon(Icons.add),
+              mini: true,
+              backgroundColor: Color(0xFF0077B6),
+            ),
+            SizedBox(height: 10),
+            FloatingActionButton(
+              onPressed: _zoomOut,
+              child: Icon(Icons.remove),
+              mini: true,
+              backgroundColor: Color(0xFF0077B6),
+            ),
+          ],
+        ),
+
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: _onItemTapped,
@@ -88,4 +163,10 @@ class _MapPageState extends State<MapPage> {
       ),
     );
   }
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
 }
